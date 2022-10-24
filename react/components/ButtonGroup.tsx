6d64f1543b6f808de0ccad726388dde0c06a26f9
jhon <estebanortizgomez96@gmail.com>
2022-10-24 11:03:13 -0500

    doc: add documentation to readme.md

--- react/components/ButtonGroup.tsx
+++ react/components/ButtonGroup.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import { useCssHandles } from 'vtex.css-handles'
 
 const ButtonGroup = () => {
+  const CSS_HANDLES = [
+    'containerButton',
+    'buttonModal',
+    'buttons',
+    'buttonModalLink',
+  ]
+
+  const handles = useCssHandles(CSS_HANDLES)
+
   return (
-    <>
-      <div>
-        <button>
-          <a href="/">Checkout</a>
+    <div className={handles.buttons}>
+      <div className={handles.containerButton}>
+        <button className={handles.buttonModal}>
+          <a className={handles.buttonModalLink} href="/">
+            Checkout
+          </a>
         </button>
       </div>
-      <div>
-        <button>Continua Comprando</button>
+      <div className={handles.containerButton}>
+        <button className={handles.buttonModal}>Continua Comprando</button>
       </div>
-    </>
+    </div>
   )
 }
 
